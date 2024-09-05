4c1cd45f230e7c5d69486faf64507077db5d1dec
Diogo Cardoso <diogoocardoso@gmail.com>
2024-09-05 19:41:38 +0100

    Removed article from draft

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725561083',resources: [
+    cacheName: 'chirpy-1725561684',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
