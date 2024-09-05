63d520ed64938ebe14443d7e7236f84705524000
Diogo Cardoso <diogoocardoso@gmail.com>
2024-09-05 19:44:42 +0100

    Re built website

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725561083',resources: [
+    cacheName: 'chirpy-1725561871',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
